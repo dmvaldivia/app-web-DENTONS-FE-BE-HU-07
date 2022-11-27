a1956121284ee07aff3445354b521153955cfec6
dmvaldivia <65515791+dmvaldivia@users.noreply.github.com>
2022-11-27 07:35:28 -0500

    version1

---
+++ ProyectoExcel/Models/DBPRUEBASContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProyectoExcel.Models
+{
+    public partial class DBPRUEBASContext : DbContext
+    {
+        public DBPRUEBASContext()
+        {
+        }
+
+        public DBPRUEBASContext(DbContextOptions<DBPRUEBASContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contacto> Contactos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=ec2-34-239-124-25.compute-1.amazonaws.com;Initial Catalog=DBPRUEBAS;Persist Security Info=True;User ID=sa;Password=M1q2w3e..");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contacto>(entity =>
+            {
+                entity.HasKey(e => e.IdContacto)
+                    .HasName("PK__CONTACTO__A4D6BBFA03F620AB");
+
+                entity.ToTable("CONTACTO");
+
+                entity.Property(e => e.Apellido)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Correo)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefono)
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
